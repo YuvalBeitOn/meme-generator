c8059e0e5b5ee82da553c91bc7229f858b33b198
Yuval Beit On <yuvalbeiton@gmail.com>
2020-10-21 20:01:41 +0300

    first-delivery

--- js/meme-controller.js
+++ js/meme-controller.js
@@ -1,1 +1,137 @@
 'use strict';
+
+var gCanvas;
+var gCtx;
+
+function init() {
+    renderGallery();
+    renderImgsGallery();
+}
+
+function renderImgsGallery() {
+    const imgs = getImgs();
+    var strHtml = imgs.map((img) => {
+        return `<img class="gallery-item" onclick="setSelectedImgId(${img.id}); renderEditor();" src="${img.url}" alt="${img.id}"/>
+        `
+    }).join('');
+    var elGallery = document.querySelector('.img-gallery');
+    elGallery.innerHTML = strHtml;
+}
+
+function initEditor() {
+    gCanvas = document.querySelector('#canvas');
+    gCtx = gCanvas.getContext('2d');
+    resizeCanvas();
+}
+
+function resizeCanvas() {
+    var elContainer = document.querySelector('.canvas-container');
+    gCanvas.width = elContainer.offsetWidth
+    gCanvas.height = elContainer.offsetHeight
+}
+
+function renderGallery() {
+    var strHtml = `<main> <section class="filters flex space-between">
+                    <input class="search-meme" type="text" placeholder="Enter search keyword">
+                    <div class="keywords"></div>
+                    </section>
+                    <div class="img-gallery flex wrap"></div>
+                    <section class="about"></section> <main/>`;
+    document.querySelector('.content').innerHTML = strHtml;
+}
+
+function renderEditor() {
+    var strHtml =
+        `<main class="editor flex">
+    <div class="canvas-container">
+        <canvas id="canvas"></canvas>
+    </div>
+    <div class="editor-controls">
+        <input class="text-input" onkeydown="handleTextChange(event)" type="text" placeholder="Enter text here">
+        <div class="btns-container">
+            <button onClick="handleChangeLine()"><img src="/img/icons/up-and-down-opposite-double-arrows-side-by-side.png" alt=""></button>
+            <button onclick="handleAddLine()"><img src="/img/icons/add.png" alt=""></button>
+            <button onclick="handleDeleteLine()"><img src="/img/icons/trash.png" alt=""></button>
+            <input onchange="handleColor(event)" type="color" id="text-color" name="text-color" value="#000000">
+        </div>
+        <div class="text-controls">
+            <button onclick="handleFontSize(${3})"><img src="/img/icons/increase font - icon.png" alt=""></button>
+            <button onclick="handleFontSize(${-3})"><img src="/img/icons/decrease font - icon.png" alt=""></button>
+            <button onclick="handleTextAlign('right')"><img src="/img/icons/align-to-left.png" alt=""></button>
+            <button onclick="handleTextAlign('left')"><img src="/img/icons/align-to-right.png" alt=""></button>
+            <button onclick="handleTextAlign('center')"><img src="/img/icons/center-text-alignment.png" alt=""></button>
+            <select class="font">
+                <option value="impact">Impact</option>
+            </select>
+            <button><img src="/img/icons/text stroke.png" alt=""></button>
+        </div>
+    </div>
+    </main>`
+    document.querySelector('.content').innerHTML = strHtml;
+    console.log(gMeme);
+    initEditor();
+    drawImg();
+}
+
+function handleColor(ev) {
+    const textColor = document.getElementById('text-color').value;
+    setColor(textColor);
+}
+
+function handleAddLine() {
+    addTextLine();
+    drawText();
+}
+
+function handleDeleteLine() {
+    deleteLine();
+    drawText();
+}
+
+function handleChangeLine() {
+    var line = getSelectedLine();
+    document.querySelector('.text-input').value = line.txt;
+    setSelectedLine();
+    drawText();
+}
+
+function handleFontSize(num) {
+    setfontSize(num);
+    drawText();
+}
+
+function handleTextAlign(align) {
+    setTextAlign(align);
+    drawText();
+}
+
+function handleTextChange(ev) {
+    setText(ev.target.value);
+    console.log(ev.target.value);
+    drawText();
+}
+
+
+function drawImg() {
+    var imgId = getSelectedImgId();
+    var img = new Image()
+    img.src = getImgById(imgId).url;
+    console.log(img.src);
+    gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height)
+}
+
+
+function drawText() {
+    const lines = getLines();
+    drawImg();
+    console.log(lines);
+    lines.forEach((line, idx) => {
+        gCtx.font = `${line.size}px Impact`;
+        gCtx.textAlign = line.align;
+        gCtx.strokeStyle = line.fill;
+        gCtx.fillStyle = line.stroke;
+        let y = (idx === 0) ? 100 : gCanvas.height - 100 + line.size;
+        gCtx.fillText(line.txt, gCanvas.width / 2, y)
+        gCtx.strokeText(line.txt, gCanvas.width / 2, y)
+    });
+}
--- js/meme-service.js
+++ js/meme-service.js
@@ -1,1 +1,112 @@
 'use strict';
+
+var gKeywords = { 'happy': 12, 'funny puk': 1 }
+var gColor = '#000000';
+
+const gImgs = [
+    { id: 1, url: 'img/1.jpg', keywords: ['happy'] },
+    { id: 2, url: 'img/2.jpg', keywords: ['happy'] },
+    { id: 3, url: 'img/3.jpg', keywords: ['happy'] },
+    { id: 4, url: 'img/5.jpg', keywords: ['happy'] },
+    { id: 5, url: 'img/6.jpg', keywords: ['happy'] },
+    { id: 6, url: 'img/7.jpg', keywords: ['happy'] },
+    { id: 7, url: 'img/8.jpg', keywords: ['happy'] },
+    { id: 8, url: 'img/9.jpg', keywords: ['happy'] },
+    { id: 9, url: 'img/10.jpg', keywords: ['happy'] },
+    { id: 10, url: 'img/11.jpg', keywords: ['happy'] },
+    { id: 11, url: 'img/12.jpg', keywords: ['happy'] },
+    { id: 12, url: 'img/13.jpg', keywords: ['happy'] },
+    { id: 13, url: 'img/14.jpg', keywords: ['happy'] },
+    { id: 14, url: 'img/15.jpg', keywords: ['happy'] },
+    { id: 15, url: 'img/16.jpg', keywords: ['happy'] },
+    { id: 16, url: 'img/17.jpg', keywords: ['happy'] },
+    { id: 17, url: 'img/18.jpg', keywords: ['happy'] },
+    { id: 18, url: 'img/4.jpg', keywords: ['happy'] },
+
+];
+
+var gMeme = {
+    selectedImgId: 1,
+    selectedLineIdx: 0,
+    lines: [{
+        txt: '',
+        size: 48,
+        align: 'center',
+        fill: 'white',
+        stroke: 'black',
+        position: {}
+    }]
+}
+
+function setColor(color) {
+    gMeme.lines[gMeme.selectedLineIdx].fill = color;
+    gMeme.lines[gMeme.selectedLineIdx].stroke = color;
+}
+
+
+function setfontSize(num) {
+    let currFontSize = gMeme.lines[gMeme.selectedLineIdx].size;
+    if (currFontSize + num > 80 || currFontSize < 40) return;
+    currFontSize = currFontSize + num;
+    gMeme.lines[gMeme.selectedLineIdx].size = currFontSize;
+    console.log(currFontSize);
+}
+
+function setTextAlign(align) {
+    gMeme.lines[gMeme.selectedLineIdx].align = align;
+}
+
+function addTextLine() {
+    let newLine = {
+        txt: '',
+        size: 48,
+        align: 'center',
+        fill: 'white',
+        stroke: 'black'
+    }
+    if (gMeme.lines.length <= 1) gMeme.lines.push(newLine);
+    gMeme.selectedLineIdx = gMeme.lines.length - 1;
+}
+
+function setSelectedLine() {
+    let currLine = gMeme.selectedLineIdx;
+    if (currLine === 0) currLine = 1;
+    else currLine = 0;
+    gMeme.selectedLineIdx = currLine;
+}
+
+function getSelectedImgId() {
+    return gMeme.selectedImgId;
+}
+
+function getImgs() {
+    return gImgs;
+}
+
+function getImgById(imgId) {
+    const img = gImgs.find(img => {
+        return imgId === img.id;
+    })
+    return img;
+}
+
+function setText(txt) {
+    gMeme.lines[gMeme.selectedLineIdx].txt = txt;
+}
+
+function getLines() {
+    return gMeme.lines;
+}
+
+function setSelectedImgId(imgId) {
+    gMeme.selectedImgId = imgId;
+}
+
+function getSelectedLine() {
+    return gMeme.lines[gMeme.selectedLineIdx];
+}
+
+function deleteLine() {
+    gMeme.lines.splice(gMeme.selectedLineIdx, 1);
+    gMeme.selectedLineIdx = 0;
+}
